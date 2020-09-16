284893ab4007e8e3e3cbde0dfaa97e9d6621087d
Sarvesh Singh <40350507+sarvesh371@users.noreply.github.com>
2020-09-16 10:16:24 +0530

    Adding common functions (#2)
    * structuring project
    
    * structuring project
    
    * structuring project
    
    * structuring project
    
    * structuring project
    
    * structuring project
    
    Co-authored-by: Sarvesh Singh <sarvesh.singh@decurtis.com>

--- README.md
+++ README.md
@@ -9,5 +9,10 @@ This is a framework for automation web application using python with pytest and 
 2. Select the interpreter by going into preferences 
 
 `Project Structure`
-1. Base - It contains all the web drivers, common functions and workers functions
-2. Resources - It contains all the url's, configurations which will be used throughout the project
+1. base - It contains all the web drivers, common functions and workers functions
+2. resources - It contains all the url's, configurations which will be used throughout the project
+3. pages - It contains all the pages class and their methods to implement POM
+4. tests - It contains all the flow class and their calling functions using pytest
+5. screenshots - We will store all our screenshots in this folder
+6. allure_results - folder to save our allure report
+7. conftest - as it is heart of pytest, we will keep only fixture and pytest methods there
---
+++ conftest.py
@@ -0,0 +1,263 @@
+from base.common import *
+
+logger = Logger(name="CONF_COMMON").get_logger
+
+
+def pytest_addoption(parser):
+    """
+    Adds custom command line options for running the pytest harness
+    All options will be stored in pytest config
+    :param parser:
+    :return:
+    """
+    parser.addoption("--url", action="store", default=None, help="URL")
+
+
+def pytest_sessionstart(session):
+    """
+    Hook to be executed before session starts and before collection
+    :param session:
+    :return:
+    """
+    pass
+
+
+def pytest_sessionfinish(session, exitstatus):
+    """
+    Hook to be executed after tests execution and session is about to end
+    :param session:
+    :param exitstatus:
+    :return:
+    """
+    pass
+
+
+def pytest_report_teststatus(report, config):
+    """
+    Hook for Test Status Report
+    :param report:
+    :param config:
+    :return:
+    """
+    pass
+
+
+def pytest_internalerror(excrepr, excinfo):
+    """
+    Hook if there is any internal error
+    :param excrepr:
+    :param excinfo:
+    :return:
+    """
+    pass
+
+
+def pytest_keyboard_interrupt(excinfo):
+    """
+    Hook Called when there is a Keyboard Interrupt
+    :param excinfo:
+    :return:
+    """
+    pass
+
+
+def pytest_configure(config):
+    """
+    Configuration changes for PyTest
+    :param config:
+    :return:
+    """
+    logger.debug(f"Configure PyTest Options")
+    os.environ["ROOT_PATH"] = config.rootdir.strpath
+    config.option.cacheclear = True
+    config.option.capture = "sys"  # no: for no output at all
+    config.option.clean_alluredir = True
+    config.option.color = "yes"
+    config.option.disable_warnings = True
+    config.option.instafail = True
+    config.option.failedfirst = True
+    config.option.json_report_indent = 2
+    config.option.json_report_omit = ["warnings"]
+    config.option.json_report = True
+    config.option.maxfail = 1
+    config.option.pythonwarnings = ["ignore:Unverified HTTPS request"]
+    config.option.tbstyle = "short"
+    config.option.self_contained_html = True
+    config.option.verbose = 1
+
+    if config.getoption("test_data") is None:
+        config.option.test_data = "test_data.json"
+
+    if config.getoption("allure_report_dir") is None:
+        config.option.allure_report_dir = f"allure-results"
+
+    if config.getoption("json_report_file") == ".report.json":
+        config.option.json_report_file = f"report.json"
+
+    if config.getoption("htmlpath") is None:
+        config.option.htmlpath = f"report.html"
+
+    if config.getoption("xmlpath") is None:
+        config.option.xmlpath = f"report.xml"
+
+
+def pytest_runtest_setup(item):
+    """
+    Runs Before pytest_runtest_call
+    :param item:
+    """
+    pass
+
+
+def pytest_runtest_call(item):
+    """
+    Called to execute the Test item
+    :param item:
+    :param nextitem:
+    """
+    pass
+
+
+def pytest_runtest_teardown(item, nextitem):
+    """
+    Runs after pytest_runtest_call
+    :param item:
+    :param nextitem:
+    """
+    pass
+
+
+@pytest.hookimpl(trylast=True, hookwrapper=True)
+def pytest_runtest_makereport():
+    """
+    This is a run into the report generated after a test case
+    is done executing
+    The hook here is used to add results to testrail/zephyr/reports after execution of a test case
+    :param item:
+    :param call:
+    :return:
+    """
+    pass
+
+
+@pytest.fixture(scope="session")
+def db_connect():
+    """
+    Connect to Database
+    :return:
+    """
+    logger.debug(f"Connecting to Database")
+    host = None
+    port = None
+    user = None
+    password = None
+
+    details = f"{host} {port} {user} {password}"
+    logger.debug(details)
+    try:
+        database = Database(
+            host=host,
+            username=user,
+            password=password,
+            database="postgres",
+            port=port,
+        )
+    except Exception as exp:
+        logger.error(exp)
+        raise Exception(f"Unable to connect DB {details}")
+
+    return database
+
+
+@pytest.fixture(scope="session")
+def jenkins():
+    """
+    Connect to Jenkins Fixture
+    :return:
+    """
+    logger.debug(f"Connecting to Jenkins API Server")
+    base_url = ''
+    username = ''
+    password = ''
+    jenkins = JenkinsAutomation(
+        base_url=base_url, username=username, password=password
+    )
+
+    return jenkins
+
+
+@pytest.fixture(scope="session", autouse=True)
+def test_rails(creds, request):
+    """
+    Get and fetch Test-Rail Creds
+    :param creds:
+    :param request:
+    :return:
+    """
+    logger.debug(f"Connecting to Test-Rails API Server")
+    if request.config.getoption("--test-rail"):
+        data = creds.test_rails
+        return TestRailAPI(
+            url=data.url, email=data.email, password=data.api_key, exc=True, retry=10
+        )
+    else:
+        return None
+
+
+@pytest.fixture(scope="session")
+def kafka():
+    """
+    Connect Kafka
+    :return:
+    :rtype:
+    """
+    consumer = KafkaConsumer('topicName', bootstrap_servers=['servers'])
+    return consumer
+
+
+@pytest.fixture(scope="session")
+def slack_message(request):
+    """
+    Connect to Slack API
+    :param request:
+    :return:
+    """
+    logger.debug(f"Connecting to Slack API")
+    return SlackNotification()
+
+
+@pytest.fixture(scope="session")
+def bit_bucket():
+    """
+    Fixture for Bitbucket Tokens
+    :return:
+    """
+    logger.debug(f"Connecting to BitBucket API Server")
+    return BitBucketApi()
+
+
+@pytest.fixture(scope='session')
+def web_driver():
+    """
+    Fixture to initialise the web driver
+    :return:
+    """
+    if os.environ.get('EXECUTOR_NUMBER', None):
+        driver = WebDriver(browser='chrome', remote='192.168.9.111', port='5432')
+    else:
+        driver = WebDriver(browser='chrome')
+    yield driver
+    try:
+        driver.driver.close()
+    except (Exception, ValueError):
+        pass
+
+
+@pytest.fixture(scope='session')
+def page():
+    """
+    Fixture to initialise the Page Class
+    :return:
+    """
+    named_tuple = namedtuple("page", ["page"])
+    return named_tuple
